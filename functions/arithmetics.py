e861c18026e53d39fb112a2871cd9d57a6e452fc
Conclure <63319713+Conclure@users.noreply.github.com>
2021-11-20 16:01:45 +0000

    Code clean up

--- functions/arithmetics.py
+++ functions/arithmetics.py
@@ -1,36 +1,38 @@
 #Benjamin är välkommen att lägga till sin kod här, glöm inte att lägga in det i en funktion som heter arit_run
 
 def arit_run():
+  print_answer = lambda answer: print(f"\nAnswer:\n{answer}")
+
   #Stringen som är en beräkning från användaren
-  Beräkning = input("Skriv den aritmetiska beräkningen du vill göra:\n")
+  prompted_calculation = input("Skriv den aritmetiska beräkningen du vill göra:\n")
 
   #Beräkningsformatisering/standardisering för python
-  if "," in Beräkning:
-    Beräkning = Beräkning.replace(",",".")
+  if "," in prompted_calculation:
+    prompted_calculation = prompted_calculation.replace(",",".")
 
   #Den faktiska beräkningen samt output printen för respektive aritmetisk operation
     #Partition delar upp en string i en lista med olika entries baserad på stringen man specificerar (före, specificerad, efter).
 
   #För division
-  if "/" in Beräkning:
-    Output = float(Beräkning.partition('/')[0]) / float(Beräkning.partition('/')[2])
+  if "/" in prompted_calculation:
+    output = float(prompted_calculation.partition('/')[0]) / float(prompted_calculation.partition('/')[2])
 
-    print("\nAnswer:\n{}".format(Output))
+    print_answer(output)
 
-  elif "*" in Beräkning:
-    Output = float(Beräkning.partition('*')[0]) * float(Beräkning.partition('*')[2])
+  elif "*" in prompted_calculation:
+    output = float(prompted_calculation.partition('*')[0]) * float(prompted_calculation.partition('*')[2])
 
-    print("\nAnswer:\n{}".format(Output))
+    print_answer(output)
 
-  elif "-" in Beräkning:
-    Output = float(Beräkning.partition('-')[0]) - float(Beräkning.partition('-')[2])
+  elif "-" in prompted_calculation:
+    output = float(prompted_calculation.partition('-')[0]) - float(prompted_calculation.partition('-')[2])
 
-    print("\nAnswer:\n{}".format(Output))
+    print_answer(output)
 
-  elif "+" in Beräkning:
-    Output = float(Beräkning.partition('+')[0]) + float(Beräkning.partition('+')[2])
+  elif "+" in prompted_calculation:
+    output = float(prompted_calculation.partition('+')[0]) + float(prompted_calculation.partition('+')[2])
 
-    print("\nAnswer:\n{}".format(Output))
+    print_answer(output)
 
   #Del som hanterar när man skrivit fel input
   else:
--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 import os
 from functions.arithmetics import arit_run
 from functions.quadratics import quad_run
+from typing import List, Dict
 
+def main() -> None:
+  #Dictionary where the name of a feature corresponds to its respective function.
+  functions_dict: Dict[str,Callable[[],None]] = {
+    "Arithmetic Calculator": arit_run,
+    "Quadratic Equation Calculator": quad_run,
+  }
 
+  keys: List[str] = list(functions_dict.keys())
 
-#Dictionary where the name of a feature corresponds to its respective function.
-functions = {
-"Arithmetic Calculator": arit_run,
-"Quadratic Equation Calculator": quad_run,
-}
-keys = list(functions.keys())
+  def prompt_welcome_message() -> None:
+    print("Hej och välkommen till Roccos fantastiska")
+    print("kalkylator, med stöd för massor av roliga")
+    print("uträkningar!")
+    print("")
+    print("För nuvarande har programmet dessa funktioner:")
 
-print("Hej och välkommen till Roccos fantastiska kalkylator, med stöd för massor av roliga uträkningar")
-print("För nuvarande har programmet dessa funktioner:")
-print(*keys, "\n")
+    for key in keys:
+      print(f"- {key}")
 
-while True:
-  func_choice = input("Vänligen skriv vilken funktion du vill använda (Enter avbryter)")
-  if func_choice == "":
+    new_line = "\n"
+    print(new_line)
+
+  def prompt_input() -> str:
+    return input("Skriv in vilken funktion du vill använda (Enter avbryter): ")
+
+  def clear() -> None:
     os.system("clear")
+
+  def prompt_cancelation() -> None:
     print("Programmet avbrutet, ha en bra dag!")
-    break
-  elif func_choice in functions:
-    os.system("clear")
-    print("Du har valt:", func_choice) 
-    functions[func_choice]() #The function corresponding to the written name will be called.
-  else:
-    print("Den inskrivna funktionen stöds inte i programmet")
-#If the function is not found in the dictionary, this message is returned and the loop continues
+
+  def prompt_choice(choice: str) -> None:
+    print("Du har valt:", choice) 
+
+  def prompt_unsupported() -> None:
+    print("Den inskrivna funktionen stöds inte i programmet!")
+
+  prompt_welcome_message()
+
+  while True:
+    func_choice: str = prompt_input()
+    if func_choice == "":
+      clear()
+      prompt_cancelation()
+      break
+    elif func_choice in functions:
+      clear()
+      prompt_choice(func_choice)
+      functions[func_choice]() #The function corresponding to the written name will be called.
+    else:
+      prompt_unsupported()
+  #If the function is not found in the dictionary, this message is returned and the loop continues
+
+if __name__ == "__main__":
+  main()
